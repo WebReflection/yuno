e7f3f4deb4cc0a120c65f396bc216773abef6751
Andrea Giammarchi <gimmarch@Andrea-Giammarchi.local>
2012-01-22 13:24:25 +0100

    fixed typo in the require

--- tests/node.js
+++ tests/node.js
@@ -4,14 +4,14 @@
   console.log(myModule.throughAdd); // true
   console.log(myModule.directly);   // true
  */
-require("yunode").use(
+require("yuno").use(
   "path"
 ).and(function (Path) {
 
   // handy private scope, reusable for both web and node
 
   // standard way to add through yuno object
-  yuno.add(exports, "throughAdd", {value: true});
+  yuno.add(exports, "throughAdd", {enumerable: true, value: true});
   // backward compatible with node exports too
   exports.directly = true;
 
